c61b3b9dacbd1daf7add3ccebddda94003d3fbe2
Vika Ruda <rudaviktoria0@gmail.com>
2024-10-21 11:42:28 +0300

    Solution 1141

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,85 @@
 'use strict';
 
-// write code here
+const getTableHeader = document.querySelectorAll('table th');
+const getTBody = document.querySelectorAll('tbody tr');
+
+getTableHeader.forEach((header) => {
+  header.addEventListener('click', function (event) {
+    const getClickOnHeader = event.target.closest('th');
+
+    if (getClickOnHeader && header.textContent === 'Name') {
+      const arrForName = [];
+
+      // Для імен
+      getTBody.forEach((itemName) => {
+        const getCellersName = itemName.cells[0];
+
+        arrForName.push(getCellersName.textContent);
+      });
+
+      arrForName.sort();
+
+      // Оновлюємо першу колонку таблиці
+      getTBody.forEach((itemName, index) => {
+        const getCellersName = itemName.cells[0];
+
+        getCellersName.textContent = arrForName[index] || ''; // Оновлюємо текст комірки
+      });
+    }
+
+    if (getClickOnHeader && header.textContent === 'Position') {
+      // Для позиції
+      const arrPosition = [];
+
+      getTBody.forEach((itemPosition) => {
+        const getCellersPosition = itemPosition.cells[1];
+
+        arrPosition.push(getCellersPosition.textContent);
+      });
+
+      arrPosition.sort();
+
+      getTBody.forEach((itemPosition, index) => {
+        const getCellersPosition = itemPosition.cells[1];
+
+        getCellersPosition.textContent = arrPosition[index] || '';
+      });
+    }
+
+    if (getClickOnHeader && header.textContent === 'Age') {
+      const arrAge = [];
+
+      getTBody.forEach((itemAge) => {
+        const getCellersAge = itemAge.cells[2];
+
+        arrAge.push(getCellersAge.textContent);
+      });
+
+      arrAge.sort();
+
+      getTBody.forEach((itemAge, index) => {
+        const getCellersAge = itemAge.cells[2];
+
+        getCellersAge.textContent = arrAge[index];
+      });
+    }
+
+    if (getClickOnHeader && header.textContent === 'Salary') {
+      const arraySalary = [];
+
+      getTBody.forEach((itemSalary) => {
+        const getCellersSalary = itemSalary.cells[3];
+
+        arraySalary.push(getCellersSalary.textContent);
+      });
+
+      arraySalary.sort((a, b) => (+a) - (+b));
+
+      getTBody.forEach((itemSalary, index) => {
+        const getCellersSalary = itemSalary.cells[3];
+
+        getCellersSalary.textContent = arraySalary[index];
+      });
+    }
+  });
+});
